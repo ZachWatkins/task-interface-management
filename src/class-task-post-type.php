962680e7609fa10dc0fe8d48c97ea117c1e617df
Zachary Watkins <watkinza@gmail.com>
2021-09-07 14:54:07 -0500

    Applying automatic changes required from WordPress Coding Standards

--- src/class-task-post-type.php
+++ src/class-task-post-type.php
@@ -48,32 +48,32 @@ class Register_Task_Post_Type {
 
 		// Register taxonomies.
 		$type_tax_args = array(
-			'description' => 'The scope of operations the task will be performed within.',
-			'labels' => array(
+			'description'       => 'The scope of operations the task will be performed within.',
+			'labels'            => array(
 				'name'         => 'Task Type',
 				'search_items' => __( 'Search Task Types', 'task-interface-management-textdomain' ),
 				'all_items'    => __( 'All Task Types', 'task-interface-management-textdomain' ),
 				'menu_name'    => 'Type',
 			),
-			'default_term' => array(
+			'default_term'      => array(
 				'name'        => 'General',
 				'slug'        => 'general',
 				'description' => 'The scope of this task will be general.',
 			),
-			'capabilities'      => array (
+			'capabilities'      => array(
 				'manage_terms' => 'manage_task_types',
 				'edit_terms'   => 'manage_task_types',
 				'delete_terms' => 'manage_task_types',
 				'assign_terms' => 'edit_tasks',
 			),
-			'args' => array(
-				'orderby' => 'name',
+			'args'              => array(
+				'orderby'                => 'name',
 				'update_term_meta_cache' => false,
 			),
-			'public' => false,
-			'show_ui' => true,
-            'show_in_rest' => false, // Todo: Make this available if integrating external timekeeping apps.
-			'show_tagcloud' => false,
+			'public'            => false,
+			'show_ui'           => true,
+			'show_in_rest'      => false, // Todo: Make this available if integrating external timekeeping apps.
+			'show_tagcloud'     => false,
 			'show_admin_column' => true,
 		);
 		new \Task_Interface_Management\Taxonomy(
@@ -85,32 +85,32 @@ class Register_Task_Post_Type {
 
 		// Register taxonomies.
 		$status_tax_args = array(
-			'description' => 'The task\'s current status of activity.',
-			'labels' => array(
+			'description'       => 'The task\'s current status of activity.',
+			'labels'            => array(
 				'name'         => 'Task Status',
 				'search_items' => __( 'Search Task Statuses', 'task-interface-management-textdomain' ),
 				'all_items'    => __( 'All Task Statuses', 'task-interface-management-textdomain' ),
 				'menu_name'    => 'Status',
 			),
-			'default_term' => array(
+			'default_term'      => array(
 				'name'        => 'To do',
 				'slug'        => 'todo',
 				'description' => 'This task has not begun.',
 			),
-			'capabilities'      => array (
+			'capabilities'      => array(
 				'manage_terms' => 'manage_task_statuses',
 				'edit_terms'   => 'manage_task_statuses',
 				'delete_terms' => 'manage_task_statuses',
 				'assign_terms' => 'edit_tasks',
 			),
-			'args' => array(
-				'orderby' => 'name',
+			'args'              => array(
+				'orderby'                => 'name',
 				'update_term_meta_cache' => false,
 			),
-			'public' => false,
-			'show_ui' => true,
-            'show_in_rest' => false, // Todo: Make this available if integrating external timekeeping apps.
-			'show_tagcloud' => false,
+			'public'            => false,
+			'show_ui'           => true,
+			'show_in_rest'      => false, // Todo: Make this available if integrating external timekeeping apps.
+			'show_tagcloud'     => false,
 			'show_admin_column' => true,
 		);
 		new \Task_Interface_Management\Taxonomy(
@@ -122,32 +122,32 @@ class Register_Task_Post_Type {
 
 		// Register taxonomies.
 		$priority_tax_args = array(
-			'description' => 'The task\'s priority level amongst other work to be done.',
-			'labels' => array(
+			'description'       => 'The task\'s priority level amongst other work to be done.',
+			'labels'            => array(
 				'name'         => 'Task Priority',
 				'search_items' => __( 'Search Task Priorities', 'task-interface-management-textdomain' ),
 				'all_items'    => __( 'All Task Priorities', 'task-interface-management-textdomain' ),
 				'menu_name'    => 'Priority',
 			),
-			'default_term' => array(
+			'default_term'      => array(
 				'name'        => 'Medium',
 				'slug'        => 'medium',
 				'description' => 'This task is of normal priority.',
 			),
-			'capabilities' => array (
+			'capabilities'      => array(
 				'manage_terms' => 'manage_task_priorities',
 				'edit_terms'   => 'manage_task_priorities',
 				'delete_terms' => 'manage_task_priorities',
 				'assign_terms' => 'edit_tasks',
 			),
-			'args' => array(
-				'orderby' => 'name',
+			'args'              => array(
+				'orderby'                => 'name',
 				'update_term_meta_cache' => false,
 			),
-			'public' => false,
-			'show_ui' => true,
-            'show_in_rest' => false, // Todo: Make this available if integrating external timekeeping apps.
-			'show_tagcloud' => false,
+			'public'            => false,
+			'show_ui'           => true,
+			'show_in_rest'      => false, // Todo: Make this available if integrating external timekeeping apps.
+			'show_tagcloud'     => false,
 			'show_admin_column' => true,
 		);
 		new \Task_Interface_Management\Taxonomy(
@@ -196,36 +196,36 @@ class Register_Task_Post_Type {
 		);
 
 		/* Register post type */
-        $args = array(
+		$args = array(
 			'labels'           => $labels,
-            'description'      => 'A way to record, organize, and complete work.',
-            'public'           => false,
+			'description'      => 'A way to record, organize, and complete work.',
+			'public'           => false,
 			'position'         => 0,
-            'hierarchical'     => true,
-            'show_ui'          => true,
-            'show_in_rest'     => false, // Todo: Make this available if integrating external timekeeping apps.
-            'menu_icon'        => 'dashicons-editor-ol',
-            'capability_type'  => 'task',
-            'map_meta_cap'     => true,
-            'supports'         => array(
-                'title',
-                'editor',
-                'author',
-            ),
+			'hierarchical'     => true,
+			'show_ui'          => true,
+			'show_in_rest'     => false, // Todo: Make this available if integrating external timekeeping apps.
+			'menu_icon'        => 'dashicons-editor-ol',
+			'capability_type'  => 'task',
+			'map_meta_cap'     => true,
+			'supports'         => array(
+				'title',
+				'editor',
+				'author',
+			),
 			'taxonomies'       => array(
 				'task-type',
 				'task-status',
 				'task-priority',
 			),
-            'has_archive'      => true,
-            'can_export'       => true,
-            'delete_with_user' => false,
+			'has_archive'      => true,
+			'can_export'       => true,
+			'delete_with_user' => false,
 			'rewrite'          => array(
 				'slug'       => $this->post_type,
 				'with_front' => true,
-				'feeds'      => false
+				'feeds'      => false,
 			),
-        );
+		);
 
 		register_post_type( $this->post_type, $args );
 
